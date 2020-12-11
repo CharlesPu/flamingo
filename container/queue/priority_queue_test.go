f31ed819fa898287f8ead492750ad27feb7fff86
Rui Pu <pu17rui@sina.com>
2020-12-11 21:00:51 +0800

    Update priority_queue_test.go

--- container/queue/priority_queue_test.go
+++ container/queue/priority_queue_test.go
@@ -8,13 +8,18 @@ func TestNewMaxPriorityQueue(t *testing.T) {
 	q := NewMaxPriorityQueue(11, func(i interface{}, j interface{}) bool {
 		return i.(int) < j.(int)
 	})
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 9; i++ {
 		if 10-i == 8 {
 			continue
 		}
 		q.Insert(10 - i)
 	}
 	q.Insert(8)
+	t.Log(q.(*priorityQueueMax).q)
+	t.Log(q.Peek())
+	q.SwapTop(1)
 
 	t.Log(q.(*priorityQueueMax).q)
+	t.Log(q.Peek())
+	t.Log(q.Pop(), q)
 }
