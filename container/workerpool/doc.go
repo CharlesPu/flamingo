b014312c829b5ed3022858ed700060930bb74fdf
Rui Pu <pu17rui@sina.com>
2021-02-14 22:01:42 +0800

    Update doc.go

--- container/workerpool/doc.go
+++ container/workerpool/doc.go
@@ -5,4 +5,9 @@ package workerpool
 // 2.https://github.com/Jeffail/tunny/blob/master/tunny.go
 // 3.https://github.com/panjf2000/ants/blob/master/pool.go
 
-// 与ants库性能上略逊 9% ，观察CPU消耗主要在worker的select轮询上。有很多优化点可以钻研
+// 在性能上逊于ants库 10% ，观察CPU消耗主要在worker的select轮询上。
+// 有一些优化点和设计方案改良点可以钻研：
+// 1.干掉worker的退出chan，减少select的轮询
+// 2.将task初始化池时固定下来
+// 3.不采用worker的竞争机制，这样就多了个compete chan的轮询
+// 4.worker的清理上可简单采取存活时间计量，而不是活跃时间
